22e7b37bd351a5559129bfa27bc7e2fcb8c68903
bttf <adnanchowdhury88@gmail.com>
2013-11-20 05:11:28 +0000

    fixed modulepath in generate_puppet_conf.sh

--- bootstrap/ubuntu-precise/generate_puppet_conf.sh
+++ bootstrap/ubuntu-precise/generate_puppet_conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR=$(cd $(dirname "$0"); pwd)
-echo "[main]" > puppet.conf.example
-echo "server=deathstar01" >> puppet.conf.example
-echo "runinterval=900" >> puppet.conf.example
-echo "pluginsync=true" >> puppet.conf.example
-echo "report=true" >> puppet.conf.example
-echo "modulepath=/etc/puppet/modules:/usr/share/puppet/modules:$DIR/../../modules" >> puppet.conf.example
+echo "[main]" > $DIR/puppet.conf.example
+echo "server=deathstar01" >> $DIR/puppet.conf.example
+echo "runinterval=900" >> $DIR/puppet.conf.example
+echo "pluginsync=true" >> $DIR/puppet.conf.example
+echo "report=true" >> $DIR/puppet.conf.example
+echo "modulepath=$DIR/../../modules:/etc/puppet/modules:/usr/share/puppet/modules" >> $DIR/puppet.conf.example
