06ed7a12831c01e7eea7fab6f93be5edb4304676
bttf <adnanchowdhury88@gmail.com>
2014-07-13 02:39:01 -0400

    added obligatory -y to apt-get install

--- bootstrap_puppetmaster.sh
+++ bootstrap_puppetmaster.sh
@@ -3,4 +3,4 @@ CODENAME=`lsb_release -a | grep Codename | cut -f2`
 wget http://apt.puppetlabs.com/puppetlabs-release-$CODENAME.deb
 dpkg -i puppetlabs-release-$CODENAME.deb
 apt-get update
-apt-get install puppetmaster-passenger
+apt-get install -y puppetmaster-passenger
