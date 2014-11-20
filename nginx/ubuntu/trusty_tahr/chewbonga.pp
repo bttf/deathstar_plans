28bd694c47148dd5ceabb91ff54347826f0e3350
bttf <adnanchowdhury88@gmail.com>
2014-11-20 15:28:17 -0500

    chewbonga blog to blog.chewbonga.com; added chewbonga.com

--- nginx/ubuntu/trusty_tahr/chewbonga.pp
+++ nginx/ubuntu/trusty_tahr/chewbonga.pp
@@ -1,27 +1,38 @@
+$chewbonga_dir = '/home/bttf/chewbonga.com'
 $backend_port = '1212'
 $frontend_dir = '/home/bttf/ember-blog/dist'
 
 include nginx
 
 nginx::resource::vhost {'chewbonga.com':
+  ensure         => present,
+  server_name    => ['chewbonga.com', 'www.chewbonga.com'],
+  listen_options => 'default_server',
+  www_root       => $chewbonga_dir,
+  raw_append     => ['location ~ /\.git {',
+                  'deny all;',
+                  '}'],
+}
+
+nginx::resource::vhost {'blog.chewbonga.com':
   ensure        => present,
-  server_name   => ['chewbonga.com', 'www.chewbonga.com', '*.chewbonga.com'],
+  server_name   => ['blog.chewbonga.com', 'www.blog.chewbonga.com'],
   try_files     => ['$uri', '$uri/', '/index.html?/$request_uri'],
-  www_root      => "${frontend_dir}",
+  www_root      => $frontend_dir,
 }
 
 nginx::resource::location {'dot-git':
   ensure              => present,
-  vhost               => 'chewbonga.com',
+  vhost               => 'blog.chewbonga.com',
   location            => '~ /\.git',
   location_custom_cfg => {
     deny              => 'all',
   }
 }
 
-nginx::resource::location {'chewbonga.com-api':
+nginx::resource::location {'blog.chewbonga.com-api':
   ensure   => present,
-  vhost    => 'chewbonga.com',
+  vhost    => 'blog.chewbonga.com',
   location => '~ ^/(entries|users|auth|logout|validate)',
   proxy    => 'http://proxypass',
 }
