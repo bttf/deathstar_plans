b72df0f456568dea25954b52d88860381baf5eec
bttf <adnanchowdhury88@gmail.com>
2016-01-03 07:22:11 -0500

    update screenwritr

--- nginx/ubuntu/trusty_tahr/screenwritr.pp
+++ nginx/ubuntu/trusty_tahr/screenwritr.pp
@@ -1,11 +1,11 @@
 $frontend_dir = '/home/bttf/screenwritr/dist'
-$app_name = 'screenwritr.io'
+$app_name = 'screenwritr.88mph.io'
 
 include nginx
 
 nginx::resource::vhost {$app_name:
   ensure        => present,
-  server_name   => ['screenwritr.io', 'www.screenwritr.io'],
+  server_name   => [$app_name, "www.$app_name"],
   try_files     => ['$uri', '$uri/', '/index.html?/$request_uri'],
   www_root      => $frontend_dir,
 }
