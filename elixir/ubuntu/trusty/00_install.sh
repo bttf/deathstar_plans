eb837ac70e89b4c242050da99319e94adaa1726b
bttf <adnanchowdhury88@gmail.com>
2016-05-14 03:50:55 -0400

    add elixir

---
+++ elixir/ubuntu/trusty/00_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb 
+
+dpkg -i erlang-solutions_1.0_all.deb
+
+apt-get update -y
+
+apt-get install esl-erlang elixir -y
